fa9e77b35960ffb5fe418a95e42322b86be0b6c2
anbcodes <anbcodes@protonmail.com>
2019-07-22 13:04:41 -0400

    orginized code

---
+++ src/components/Board.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout>
+    <v-flex>
+      <v-layout v-for="(row, rowIndex) in board" :key="rowIndex">
+        <v-flex v-for="(column, colIndex) in row" :key="colIndex" xs2 sm2 md2 lg2 xl2>
+          <v-card elevation-0 style="width: 100%; height: 100%">
+            <v-card-title primary-title>
+              <h5 class="subheading mb-0">{{column[0]}} ({{rowIndex*6+colIndex+1}})</h5>
+            </v-card-title>
+            <v-card-text>Translation: {{showTranslation ? column[1] : ""}}</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {
+  props: {
+    board: Array,
+    showTranslation: Boolean
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Challenge.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-layout>
+    <v-flex centered shrink mr-3 mt-1>
+      <h2 class="heading mt-2">Challenge {{name}} Weeks:</h2>
+    </v-flex>
+    <v-flex>
+      <v-range-slider
+        v-model="value[name].range"
+        :max="30"
+        :min="1"
+        :thumb-size="24"
+        :step="1"
+        thumb-label="always"
+        :disabled="!value[name].selected"
+        always-dirty
+        @change="valueChange"
+      ></v-range-slider>
+    </v-flex>
+    <v-flex shrink ml-2>
+      <v-checkbox v-model="value[name].selected" @change="valueChange" />
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+export default {
+  props: {
+    value: Object,
+    name: String
+  },
+  created() {
+    this.value[this.name] = { range: [1, 2], selected: true };
+  },
+  methods: {
+    valueChange() {
+      this.$emit("input", this.value);
+    }
+  }
+};
+</script>
